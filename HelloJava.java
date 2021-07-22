23ffac8ad4759f4c09ace7816c1560f03ba20fe9
RaikaMogultay <raika.engin@gmail.com>
2021-07-22 17:20:10 -0500

    helloworld Java

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+package Deneme;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//  Merhaba yazan Program
+		System.out.println("Hello World");
+		System.out.print ("I'm Raika");
+
+	}
+
+}
