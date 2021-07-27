d96875320c4743d29ac3c98819385fadc1dffaf5
RaikaMogultay <raika.engin@gmail.com>
2021-07-26 22:35:24 -0500

    Hello World

--- HelloJava.java
+++ HelloJava.java
@@ -4,8 +4,22 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		//  Merhaba yazan Program
-		System.out.println("Hello World");
-		System.out.print ("I'm Raika");
+		
+		String metin = "Degiskenleri ogreniyorum" ;
+		String ismim = "I'm Raika";
+		
+		char ilkHarf = 'r' ;
+		int yasim = 26;
+		double kilom = 56.5;
+		
+		String yasMetni = " yasindayim";
+		
+		System.out.println(metin + " " + ismim);
+		System.out.println("Ismimin ilk harfi : " + ilkHarf);
+		System.out.println(yasim + yasMetni);
+		System.out.println("kilom : " + kilom);
+		System.out.println(yasim + kilom);
+		
 
 	}
 
